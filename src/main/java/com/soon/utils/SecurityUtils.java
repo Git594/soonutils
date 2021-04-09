6da9a9ec4bff196167cd077d77bde067f02d1702
huyigong <huyigong>
2021-04-09 18:33:23 +0800

    开发：添加totp加密方法，完成90%

---
+++ src/main/java/com/soon/utils/SecurityUtils.java
@@ -0,0 +1,16 @@
+package com.soon.utils;
+
+import java.util.Calendar;
+
+/**
+ * 安全工具
+ *
+ * @author HuYiGong
+ * @date 2021/4/9
+ **/
+public class SecurityUtils {
+//
+//    public static void main() {
+//        Calendar.getInstance()
+//    }
+}
