70ab202d3a2148be2f9936e4eb8714c7c9a5e3db
Dale <594228031@qq.com>
2021-05-13 00:14:15 +0800

    开发：UUID生成器，完成100%

---
+++ src/main/java/com/soon/utils/UidGenerator.java
@@ -0,0 +1,21 @@
+package com.soon.utils;
+
+import java.util.UUID;
+
+/**
+ * Created on 2021/5/13.
+ *
+ * @author Soon
+ */
+public class UidGenerator {
+    /**
+     * 获取去掉"-"后的UUID
+     *
+     * @return 去掉"-"后的UUID
+     * @author HuYiGong
+     * @since 2021/5/13
+     */
+    public static String getCleanUid() {
+        return UUID.randomUUID().toString().replaceAll("-", "");
+    }
+}
