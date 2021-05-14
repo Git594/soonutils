1381e6fb524e62ea63470d270d7ffa059ce6792e
huyigong <huyigong>
2021-05-14 18:05:53 +0800

    开发：数字工具，补零方法，完成100% 开发：动态密码生成工具，完成100%

---
+++ src/main/java/com/soon/utils/NumberUtils.java
@@ -0,0 +1,30 @@
+package com.soon.utils;
+
+import java.text.NumberFormat;
+
+/**
+ * 数字工具
+ *
+ * @author HuYiGong
+ * @since 2021/5/14
+ **/
+public class NumberUtils {
+    private NumberUtils() {}
+
+    /**
+     * 补零
+     *
+     * @param number 数字
+     * @param digit 位数
+     * @return java.lang.String 补零后的字符串
+     * @author HuYiGong
+     * @date 2021/5/14 16:40
+     */
+    public static String fillZero(long number, int digit) {
+        NumberFormat format = NumberFormat.getInstance();
+        format.setGroupingUsed(false);
+        format.setMinimumIntegerDigits(digit);
+        format.setMaximumIntegerDigits(digit);
+        return format.format(number);
+    }
+}
--- src/main/java/com/soon/utils/security/Totp.java
+++ src/main/java/com/soon/utils/security/Totp.java
@@ -1,11 +1,13 @@
 package com.soon.utils.security;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
-import java.security.Key;
 import java.security.NoSuchAlgorithmException;
+import java.text.NumberFormat;
 
 /**
  * TOTP: Time-Based One-Time Password Algorithm
@@ -31,27 +33,51 @@ public class Totp {
     /**
      * 安全秘钥（K）
      */
-    private String securityKey;
+    private final String securityKey;
 
     /**
      * 自定义时间（T0）
      */
-    private long customizeTime;
+    private final long customizeTime;
 
     /**
      * 步长（X）
      */
-    private int step;
+    private final int step;
 
     /**
      * 生成的密码位数（Digit）
      */
-    private int digit;
+    private final int digit;
 
     /**
      * 柔性时间回溯
      */
-    private int flexTime;
+    private final int flexedTime;
+
+    /**
+     * 柔性验证
+     *
+     * @param userName 用户名
+     * @param password 密码
+     * @param code 动态密码
+     * @return boolean
+     * @author HuYiGong
+     * @date 2021/5/14 17:21
+     */
+    public boolean verifyFlexibly(String userName, String password, String code) {
+        Totp totp = getInstant(securityKey + userName + password);
+        try {
+            if (totp.generateFlexibly().equals(code)) {
+                return true;
+            } else {
+                return totp.generate().equals(code);
+            }
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 
     /**
      * 获取实例
@@ -62,7 +88,7 @@ public class Totp {
      * @author HuYiGong
      * @since 2021/4/9 16:25
      */
-    public Totp getInstant(String securityKey) {
+    public static Totp getInstant(String securityKey) {
         return new Totp(securityKey, 0, 30000, 6, 5000);
     }
 
@@ -77,7 +103,7 @@ public class Totp {
      * @author HuYiGong
      * @since 2021/4/9 16:31
      */
-    public Totp getInstant(String securityKey, int step, int digit) {
+    public static Totp getInstant(String securityKey, int step, int digit) {
         if (step == 0) {
             throw new IllegalArgumentException("Step cannot be 0");
         }
@@ -96,7 +122,7 @@ public class Totp {
      * @author HuYiGong
      * @since 2021/4/9 16:33
      */
-    public Totp getInstant(String securityKey, int step, int digit, int flexTime, long customizeTime) {
+    public static Totp getInstant(String securityKey, int step, int digit, int flexTime, long customizeTime) {
         if (step == 0) {
             throw new IllegalArgumentException("Step cannot be 0");
         }
@@ -118,7 +144,29 @@ public class Totp {
     }
 
     /**
+     * 生成柔性totp
+     *
+     * @return java.lang.String
+     *          totp结果
+     * @author HuYiGong
+     * @date 2021/4/9 18:31
+     */
+    public String generateFlexibly() throws InvalidKeyException {
+        long timeFactor = computeTimeFactor(System.currentTimeMillis() - flexedTime);
+        byte[] hmacSha1Res = hmacSha1(securityKey, String.valueOf(timeFactor));
+        return truncate(hmacSha1Res);
+    }
+
+    /**
      * 截断
+     *
+     * 先从第一步通过 SHA-1 算法加密得到的 20 字节长度的结果中选取最后一个字节的低字节位的 4 位（注意：动态密码算法中采用的大
+     * 端(big-endian)存储）；
+     * 将这 4 位的二进制值转换为无标点数的整数值，得到 0 到 15（包含 0 和 15）之间的一个数，这个数字作为 20 个字节中从 0 开始
+     * 的偏移量；
+     * 接着从指定偏移位开始，连续截取 4 个字节（32 位），最后返回 32 位中的后面 31 位。
+     * 回到算法本身，在获得 31 位的截断结果之后，我们将其又转换为无标点的整数值，这个值的取值范围是 0 ~ 2^31，也即
+     * 0 ~ 2.147483648E9，最后我们将这个数对10的乘方（digit 指数范围 1-10）取模，得到一个余值，对其前面补0得到指定位数的字符串
      *
      * @param hmacSha1Res HmacSHA1加密后的记过
      * @return java.lang.String
@@ -127,7 +175,15 @@ public class Totp {
      * @since 2021/4/9 18:29
      */
     private String truncate(byte[] hmacSha1Res) {
-        return null;
+        int offset = hmacSha1Res[hmacSha1Res.length - 1] & 0xf;
+        long truncation = (hmacSha1Res[offset] & 0x7f) << 24 | (hmacSha1Res[offset + 1] & 0xff) << 16
+                | (hmacSha1Res[offset + 2] & 0xff) << 8 | hmacSha1Res[offset + 3] & 0xff;
+        long mold = (long) (truncation % Math.pow(10, digit));
+        NumberFormat format = NumberFormat.getInstance();
+        format.setGroupingUsed(false);
+        format.setMinimumIntegerDigits(digit);
+        format.setMaximumIntegerDigits(digit);
+        return format.format(mold);
     }
 
     /**
@@ -164,11 +220,11 @@ public class Totp {
         return (targetTime - customizeTime) / step;
     }
 
-    private Totp(String securityKey, long customizeTime, int step, int digit, int flexTime) {
+    private Totp(String securityKey, long customizeTime, int step, int digit, int flexedTime) {
         this.securityKey = securityKey;
         this.customizeTime = customizeTime;
         this.step = step;
         this.digit = digit;
-        this.flexTime = flexTime;
+        this.flexedTime = flexedTime;
     }
 }
