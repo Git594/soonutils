ac8613ba494ed14d842235ca38a2ed2c45773d0e
huyigong <huyigong>
2021-06-16 21:59:20 +0800

    开发：新增Aes加解密，返回byte数组

--- src/main/java/com/soon/utils/security/Aes.java
+++ src/main/java/com/soon/utils/security/Aes.java
@@ -2,7 +2,10 @@ package com.soon.utils.security;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
-import java.nio.charset.StandardCharsets;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Base64;
 
 /**
@@ -17,16 +20,16 @@ public class Aes {
     /**
      * 使用KeyGenerator双向加密，AES，注意这里转化为字符串的时候是用Base64进行加解密
      *
-     * @param content 加密的原文
-     * @param key 加密的秘钥
+     * @param content 加密或解密的原文
+     * @param key 秘钥
      * @param isEncode 是否加密
      * @return java.lang.String
      * @author HuYiGong
      * @since 2021/6/16 18:50
      */
-    private static String generateAes(String content, String key, boolean isEncode) {
+    private static String generateBase64Aes(String content, String key, boolean isEncode) {
         try {
-            SecretKeySpec sks = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "AES");
+            SecretKeySpec sks = new SecretKeySpec(key.getBytes(), "AES");
             Cipher cipher = Cipher.getInstance("AES");
             if (isEncode) {
                 cipher.init(Cipher.ENCRYPT_MODE, sks);
@@ -42,16 +45,43 @@ public class Aes {
     }
 
     /**
+     * 使用KeyGenerator双向加密，AES
+     *
+     * @param bytes 加密或解密的字节数组
+     * @param key 秘钥
+     * @param isEncode 是否加密
+     * @return java.lang.String
+     * @author HuYiGong
+     * @since 2021/6/16 18:50
+     */
+    private static byte[] generateAes(byte[] bytes, String key, boolean isEncode) {
+        try {
+            SecretKeySpec sks = new SecretKeySpec(key.getBytes(), "AES");
+            Cipher cipher = Cipher.getInstance("AES");
+            if (isEncode) {
+                cipher.init(Cipher.ENCRYPT_MODE, sks);
+                return cipher.doFinal(bytes);
+            } else {
+                cipher.init(Cipher.DECRYPT_MODE, sks);
+                return cipher.doFinal(bytes);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new byte[0];
+        }
+    }
+
+    /**
      * 使用AES加密算法经行加密（可逆）
      *
      * @param content 需要加密的密文
      * @param key 秘钥
-     * @return java.lang.String
+     * @return 加密后的密文
      * @author HuYiGong
      * @since 2021/6/16 19:43
      */
     public static String encode(String content, String key) {
-        return generateAes(content, key, true);
+        return generateBase64Aes(content, key, true);
     }
 
     /**
@@ -59,11 +89,38 @@ public class Aes {
      *
      * @param content 需要解密的密文
      * @param key 秘钥
-     * @return java.lang.String
+     * @return 原文
      * @author HuYiGong
      * @since 2021/6/16 19:43
      */
     public static String decode(String content, String key) {
-        return generateAes(content, key, false);
+        return generateBase64Aes(content, key, false);
+    }
+
+
+    /**
+     * 使用AES加密算法经行加密（可逆）
+     *
+     * @param bytes 需要加密的字节数组
+     * @param key 秘钥
+     * @return 加密后的字节数组
+     * @author HuYiGong
+     * @since 2021/6/16 19:43
+     */
+    public static byte[] encode(byte[] bytes, String key) {
+        return generateAes(bytes, key, true);
+    }
+
+    /**
+     * 对使用AES加密算法的密文进行解密
+     *
+     * @param bytes 需要解密的字节数组
+     * @param key 秘钥
+     * @return 解密后的字节数组
+     * @author HuYiGong
+     * @since 2021/6/16 19:43
+     */
+    public static byte[] decode(byte[] bytes, String key) {
+        return generateAes(bytes, key, false);
     }
 }
